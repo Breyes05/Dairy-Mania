17cb934613d6c88221e34810779b099d92f3038f
Monke <dragonslayerx19@gmail.com>
2022-05-24 11:40:17 -0700

    Level 1 advancements part 2
    made marshmellow walls, and getting ready for death coding for tomorrow

---
+++ Assets/Scripts/CameraControlls.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControlls : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+
+}
